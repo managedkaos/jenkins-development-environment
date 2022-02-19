0e9a319c636540d4b1610f9a20c28b562ee60b99
ManagedKaos <managedkaos@users.noreply.github.com>
2022-02-19 00:04:29 -0800

    Create a role; attach the AmazonSSMManagedInstanceCore policy to the role; attach the role to the jenkins server

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -2,6 +2,40 @@ locals {
   jenkins_tags = merge({ Name : "jenkins-server" }, var.tags, local.tags)
 }
 
+resource "aws_iam_role" "jenkins" {
+  name_prefix = "jenkins-server-"
+  tags        = merge(var.tags, local.tags)
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins" {
+  role       = aws_iam_role.jenkins.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "jenkins" {
+  name_prefix = "jenkins-server-"
+  role        = aws_iam_role.jenkins.name
+  tags = {
+    git_file = "jenkins-server.tf"
+    git_org  = "managedkaos"
+    git_repo = "jenkins-development-environment"
+  }
+}
+
 resource "aws_security_group" "jenkins" {
   name_prefix = "jenkins-server-"
   description = "jenkins"
@@ -43,6 +77,8 @@ resource "aws_instance" "jenkins" {
   key_name                    = aws_key_pair.key["ubuntu"].key_name
   security_groups             = [aws_security_group.jenkins.name]
   volume_tags                 = local.jenkins_tags
+  iam_instance_profile        = aws_iam_instance_profile.jenkins.name
+
   tags = merge(local.jenkins_tags, {
     git_file = "jenkins-server.tf"
     git_org  = "managedkaos"
