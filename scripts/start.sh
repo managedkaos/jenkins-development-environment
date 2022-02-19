5782ee322df0f60f8c16e4c17e48ff1b703cc6cd
ManagedKaos <managedkaos@users.noreply.github.com>
2022-02-18 23:32:40 -0800

    Add a timestamp to the start/stop scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,13 +2,13 @@
 echo "# Getting targets..."
 targets="$(aws ec2 describe-instances --query 'Reservations[].Instances[][].{Instances: InstanceId}' --output text --filters "Name=tag:Designation,Values=jenkins-development-environment-production" --output=text)"
 
-echo "# Starting targets..."
+echo "# $(date) Starting targets..."
 aws ec2 start-instances --instance-ids ${targets}
 
-echo "# Waiting for targets to start..."
+echo "# $(date) Waiting for targets to start..."
 aws ec2 wait instance-running --instance-ids ${targets}
 
-echo "# Waiting for target status to be OK..."
+echo "# $(date) Waiting for target status to be OK..."
 aws ec2 wait instance-status-ok --instance-ids ${targets}
 
-echo "# Start complete!"
+echo "# $(date) Start complete!"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,10 +2,10 @@
 echo "# Getting targets..."
 targets="$(aws ec2 describe-instances --query 'Reservations[].Instances[][].{Instances: InstanceId}' --output text --filters "Name=tag:Designation,Values=jenkins-development-environment-production" --output=text)"
 
-echo "# Stopping targets..."
+echo "# $(date) Stopping targets..."
 aws ec2 stop-instances --instance-ids ${targets}
 
-echo "# Waiting for targets to stop..."
+echo "# $(date) Waiting for targets to stop..."
 aws ec2 wait instance-stopped --instance-ids ${targets}
 
-echo "# Stop complete!"
+echo "# $(date) Stop complete!"
